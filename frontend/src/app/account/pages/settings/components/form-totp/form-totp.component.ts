127b1a958452dc8034d6c0ffd0ee18cdca0857c7
shaninalex <shanin.x@gmail.com>
2023-11-25 13:04:25 +0200

    add blank components for password, totp and lookup-secret parts of settings form

---
+++ frontend/src/app/account/pages/settings/components/form-totp/form-totp.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: '#form-totp',
+    templateUrl: './form-totp.component.html',
+    encapsulation: ViewEncapsulation.None
+})
+export class FormTotpComponent {
+
+}
